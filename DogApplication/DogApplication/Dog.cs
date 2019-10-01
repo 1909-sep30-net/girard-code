5228338bcfe9d9972edbed46d767387e5074b8fb
Girard Sanford <girard.sanford@gmail.com>
2019-10-01 17:05:48 -0500

    visual studio dog app

---
+++ DogApplication/DogApplication/Dog.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DogApplication
+{
+    class Dog
+    {
+        // access modifiers
+        // public - everyone can see
+        // internal - everyone in the same assembly (aka project) can see
+        // protected - everyone who is a subclass of dog can see( even if it is in another project)
+        // private - only this class can see
+
+        //for class members, private is default.
+        //(for classes, internal is default - only public and internal are possible
+
+        //constructor
+        //there is always at least one constructor
+        // -- if you don't write one, you get a default one like "Dog()()" (zero param, zero extra behavior)
+
+        public Dog(string name, int age)
+        {
+            //within a class , "this" is a way to refer to current instacne of that class
+            
+            this.name = name;
+            if(age < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+                // an exception is how we represent and handle "errors" during runtime
+            }
+            this.age = age;
+        }
+
+        //fields
+        private int age;
+        private string name;
+
+        // property
+        //public int Age { get; set; }
+        //this is an "auto property"
+
+        public int Age
+        {
+            get
+            {
+                return age; // the backing fiels of the property
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentNullException(nameof(value));
+                }
+                age = value;
+            }
+        }
+
+
+
+        //better than public fields, is getter and setter methods like java
+        //but better than than that is properties
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void SetName()
+        {
+            //validation goes here - this is public, field is private
+            //all the consumers of this class don't have to worry about validating their changes to fields
+
+            if (name == null)
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+            this.name = name;
+        }
+    }
+}
---
+++ DogApplication/DogApplication/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DogApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog;
+            try
+            {
+                // in the try bloc, you put the code that you think may throw an exception that you can handle here
+                dog = new Dog(null, -5);
+
+                //in C#, we have "bloc scope"
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("error, recovering");
+                dog = new Dog("Spot", 5);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("error, recovering some argument error");
+                dog = new Dog("Spot", 5);
+            }
+            
+            //dog.name = "Spot";
+            //dog.age = 4;
+        }
+    }
+}
